7ee052593506e9f845fda0d1b67ec91e9e7ae873
ErrQD0303 <quocdat3396@gmail.com>
2024-03-27 17:36:27 +0700

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipe) {
   //
 };
 
+const newFeature = function () {
+  console.log("Welcome to the new application");
+};
+
 // Basically Main method
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -133,7 +137,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("Welcome");
+  newFeature();
 };
 
 // This code run the program
